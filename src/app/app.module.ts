ed7f3aa9ef2614090caa91a32c55fc7e8dfe23f1
monroekimmel <monroekimmel@gmail.com>
2021-01-20 20:34:52 -0500

    Added vendor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { UserListComponent } from './feature/user/user-list/user-list.component'
 import { UserCreateComponent } from './feature/user/user-create/user-create.component';
 import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
 import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
+import { VendorCreateComponent } from './feature/vendor/vendor-create/vendor-create.component';
+import { VendorDetailComponent } from './feature/vendor/vendor-detail/vendor-detail.component';
+import { VendorEditComponent } from './feature/vendor/vendor-edit/vendor-edit.component';
+import { VendorListComponent } from './feature/vendor/vendor-list/vendor-list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { UserEditComponent } from './feature/user/user-edit/user-edit.component'
     UserListComponent,
     UserCreateComponent,
     UserDetailComponent,
-    UserEditComponent
+    UserEditComponent,
+    VendorCreateComponent,
+    VendorDetailComponent,
+    VendorEditComponent,
+    VendorListComponent
   ],
   imports: [
     BrowserModule,
