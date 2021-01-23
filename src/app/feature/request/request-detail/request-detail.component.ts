ecbdb1948c86b5bd67fc3237fd628ffb34711842
monroekimmel <monroekimmel@gmail.com>
2021-01-23 11:18:37 -0500

    Added request-detail

--- src/app/feature/request/request-detail/request-detail.component.ts
+++ src/app/feature/request/request-detail/request-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Request } from 'src/app/model/request.class';
+import { RequestService } from 'src/app/service/request.service';
 
 @Component({
   selector: 'app-request-detail',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-detail.component.css']
 })
 export class RequestDetailComponent implements OnInit {
+  title = "Request Detail";
+  request: Request = null;
+  requestId: number = 0;
 
-  constructor() { }
+  constructor(private requestSvc: RequestService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
+    // get the id from the url
+    this.route.params.subscribe(
+      parms => {
+        this.requestId = parms['id'];
+      });
+    // get request by id
+    this.requestSvc.getById(this.requestId).subscribe(
+      resp => {
+        this.request = resp as Request;
+      },
+      err => {
+        console.log(err);
+      }
+    )
   }
 
+  delete() {
+    // delete the request from the DB
+    this.requestSvc.delete(this.request.id).subscribe(
+      resp => {
+        this.request = resp as Request;
+        // forward to the request list component
+        this.router.navigateByUrl("/request-list");
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+    
 }
