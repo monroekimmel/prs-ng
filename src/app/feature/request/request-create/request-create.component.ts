638b46b2d62bf7a0112f91b28fa63f77fd2a4adf
monroekimmel <monroekimmel@gmail.com>
2021-01-23 09:55:02 -0500

    Added request-create

--- src/app/feature/request/request-create/request-create.component.ts
+++ src/app/feature/request/request-create/request-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Request } from 'src/app/model/request.class';
+import { RequestService } from 'src/app/service/request.service';
+import { SystemService } from 'src/app/service/system.service';
 
 @Component({
   selector: 'app-request-create',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-create.component.css']
 })
 export class RequestCreateComponent implements OnInit {
+  title = "Request Create";
+  submitBtnTitle = "Create";
+  request: Request = new Request();
 
-  constructor() { }
+  constructor(private requestSvc: RequestService,
+              private sysSvc: SystemService,
+              private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  save() {
+    this.request.user = this.sysSvc.loggedInUser;
+    // save the request to the DB
+    this.requestSvc.create(this.request).subscribe(
+      resp => {
+        this.request = resp as Request;
+        console.log("Request created", this.request);
+        // forward to the request list component
+        this.router.navigateByUrl("/request-list");
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
