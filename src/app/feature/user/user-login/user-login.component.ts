f0f5b40e1e8221b39cb6c3a2f8d3399a7e7c1dea
monroekimmel <monroekimmel@gmail.com>
2021-01-25 21:06:14 -0500

    Finished up user CRUD

--- src/app/feature/user/user-login/user-login.component.ts
+++ src/app/feature/user/user-login/user-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/model/user.class';
+import { SystemService } from 'src/app/service/system.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-user-login',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
+  title: string = 'Login';
+  message: string = "";
+  user: User = new User();
 
-  constructor() { }
+  constructor(private userSvc: UserService,
+              private sysSvc: SystemService,
+              private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.userSvc.login(this.user).subscribe(
+      resp => {
+        if(resp == null) {
+          this.message = "Invalid username or password";
+        } else {
+          this.user = resp as User;
+          this.sysSvc.loggedInUser = this.user;
+          this.router.navigateByUrl("/user-list");
+        }
+      },
+      err => {
+        console.log("User login error.", err);
+        this.message = "Error during login";
+      }
+    );
   }
 
 }
