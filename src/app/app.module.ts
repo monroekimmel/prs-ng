42138e037cfa505edf7b811076e1ec69fa400736
monroekimmel <monroekimmel@gmail.com>
2021-01-25 21:01:36 -0500

    Updated modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './feature/user/user-list/user-list.component';
 import { UserCreateComponent } from './feature/user/user-create/user-create.component';
 import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
 import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
-import { VendorCreateComponent } from './feature/vendor/vendor-create/vendor-create.component';
-import { VendorDetailComponent } from './feature/vendor/vendor-detail/vendor-detail.component';
-import { VendorEditComponent } from './feature/vendor/vendor-edit/vendor-edit.component';
-import { VendorListComponent } from './feature/vendor/vendor-list/vendor-list.component';
-import { RequestCreateComponent } from './feature/request/request-create/request-create.component';
-import { RequestDetailComponent } from './feature/request/request-detail/request-detail.component';
-import { RequestEditComponent } from './feature/request/request-edit/request-edit.component';
-import { RequestListComponent } from './feature/request/request-list/request-list.component';
-import { RequestLinesComponent } from './feature/request/request-lines/request-lines.component';
-import { RequestReviewComponent } from './feature/request/request-review/request-review.component';
-import { ReviewApproveComponent } from './feature/request/review-approve/review-approve.component';
-import { RequestApproveComponent } from './feature/request/request-approve/request-approve.component';
 import { MenuComponent } from './core/menu/menu.component';
-import { NoComponentDefinedComponent } from './core/no-component-defined/no-component-defined.component';
+import { VendorListComponent } from './feature/vendor/vendor-list/vendor-list.component';
+import { VendorDetailComponent } from './feature/vendor/vendor-detail/vendor-detail.component';
+import { VendorCreateComponent } from './feature/vendor/vendor-create/vendor-create.component';
+import { VendorEditComponent } from './feature/vendor/vendor-edit/vendor-edit.component';
+import { UserLoginComponent } from './feature/user/user-login/user-login.component';
 import { ProductListComponent } from './feature/product/product-list/product-list.component';
+import { ProductDetailComponent } from './feature/product/product-detail/product-detail.component';
 import { ProductCreateComponent } from './feature/product/product-create/product-create.component';
 import { ProductEditComponent } from './feature/product/product-edit/product-edit.component';
-import { ProductDetailComponent } from './feature/product/product-detail/product-detail.component';
+import { RequestListComponent } from './feature/request/request-list/request-list.component';
+import { RequestDetailComponent } from './feature/request/request-detail/request-detail.component';
+import { RequestCreateComponent } from './feature/request/request-create/request-create.component';
+import { RequestEditComponent } from './feature/request/request-edit/request-edit.component';
+import { NoComponentDefinedComponent } from './core/no-component-defined/no-component-defined.component';
+import { RequestLinesComponent } from './feature/request/request-lines/request-lines.component';
+import { LineItemCreateComponent } from './feature/lineItem/line-item-create/line-item-create.component';
+import { LineItemEditComponent } from './feature/lineItem/line-item-edit/line-item-edit.component';
+import { RequestReviewComponent } from './feature/request/request-review/request-review.component';
+import { RequestApproveComponent } from './feature/request/request-approve/request-approve.component';
 
 @NgModule({
   declarations: [
@@ -33,28 +36,32 @@ import { ProductDetailComponent } from './feature/product/product-detail/product
     UserCreateComponent,
     UserDetailComponent,
     UserEditComponent,
-    VendorCreateComponent,
-    VendorDetailComponent,
-    VendorEditComponent,
-    VendorListComponent,
-    RequestCreateComponent,
-    RequestDetailComponent,
-    RequestEditComponent,
-    RequestListComponent,
-    RequestLinesComponent,
-    RequestReviewComponent,
-    ReviewApproveComponent,
-    RequestApproveComponent,
     MenuComponent,
-    NoComponentDefinedComponent,
+    VendorListComponent,
+    VendorDetailComponent,
+    VendorCreateComponent,
+    VendorEditComponent,
+    UserLoginComponent,
     ProductListComponent,
+    ProductDetailComponent,
     ProductCreateComponent,
     ProductEditComponent,
-    ProductDetailComponent
+    RequestListComponent,
+    RequestDetailComponent,
+    RequestCreateComponent,
+    RequestEditComponent,
+    NoComponentDefinedComponent,
+    RequestLinesComponent,
+    LineItemCreateComponent,
+    LineItemEditComponent,
+    RequestReviewComponent,
+    RequestApproveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
