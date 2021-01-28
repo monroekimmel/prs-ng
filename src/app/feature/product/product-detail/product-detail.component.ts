92faa75b7f39e27a0f0e6536480d5e44f7038544
monroekimmel <monroekimmel@gmail.com>
2021-01-27 19:58:36 -0500

    Added product CRUD

--- src/app/feature/product/product-detail/product-detail.component.ts
+++ src/app/feature/product/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/model/product.class';
+import { ProductService } from 'src/app/service/product.service';
+import { SystemService } from 'src/app/service/system.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
-  constructor() { }
+  title = "Product Detail";
+  product: Product = null;
+  productId: number = 0;
+  
+  constructor(private productSvc: ProductService,
+              private sysSvc: SystemService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.sysSvc.checkLogin();
+
+    this.route.params.subscribe(
+      parms => {
+        this.productId = parms['id'];
+      });
+    this.productSvc.getById(this.productId).subscribe(
+      resp => {
+        this.product = resp as Product;
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+  delete() {
+    this.productSvc.delete(this.product.id).subscribe(
+      resp => {
+        this.product = resp as Product;
+        this.router.navigateByUrl("/product-list");
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
--- src/app/feature/product/product-list/product-list.component.ts
+++ src/app/feature/product/product-list/product-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product.class';
+import { SystemService } from 'src/app/service/system.service';
+import { ProductService } from '../../../service/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  title = "Product List";
+  products: Product[] = [];
+  isNotAdmin = false;
 
-  constructor() { }
+  constructor(private productSvc: ProductService,
+              private sysSvc : SystemService) { }
 
   ngOnInit(): void {
+    this.sysSvc.checkLogin();
+
+    if(!(this.sysSvc.loggedInUser.admin)) {
+      this.isNotAdmin = true;
+    }
+
+    this.productSvc.getAll().subscribe(
+      resp => {
+        this.products = resp as Product[];
+      },
+      err => {
+        console.log(err);
+      }
+    )
   }
 
 }
